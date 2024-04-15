0c0f22136b47af12888e978551c9cb252d105fd2
IgorAngelotti <lanzaigor@live.com>
2024-04-15 15:03:10 -0300

    primeiro commit

--- Aula1204Exercicio/Program.cs
+++ Aula1204Exercicio/Program.cs
@@ -12,25 +12,25 @@ namespace Aula1204Exercicio
     {
         public class Elevador
         {
-            private int terreo {  get; set; }  
+            private int andarAtual {  get; set; }  
 
             private int totalAndares { get; set; }
 
-            private int capacidade { get; set; }
+            private int totalPessoas { get; set; }
 
             private int pessoasDentro { get; set; }
 
             public Elevador(int capacidadeElevador, int totalDeAndares) 
             {              
                 this.totalAndares = totalDeAndares;   
-                this.capacidade = capacidadeElevador; 
+                this.totalPessoas = capacidadeElevador; 
                 this.pessoasDentro = 0;
-                this.terreo = 0;
+                this.andarAtual= 0;
             }
 
             public void entrar()
             {
-                if (this.pessoasDentro < this.capacidade)
+                if (this.pessoasDentro < this.totalPessoas)
                 {
                     this.pessoasDentro++;
                     Console.WriteLine($"Pessoa adicionada.");
@@ -56,9 +56,9 @@ namespace Aula1204Exercicio
 
             public void sobe()
             {
-                if (this.terreo != this.totalAndares)
+                if (this.andarAtual != this.totalAndares)
                 {
-                    this.terreo++;
+                    this.andarAtual++;
                     Console.WriteLine($"Elevador subiu um andar.");
                 }
                 else
@@ -69,9 +69,9 @@ namespace Aula1204Exercicio
 
             public void desce()
             {
-                if (this.terreo != 0)
+                if (this.andarAtual != 0)
                 {
-                    this.terreo--;
+                    this.andarAtual--;
                     Console.WriteLine($"Elevador desceu um andar.");
                     // ou Console.ReadKey() para caso usar o do while e o clear apagar essa mensagem (clear antes do "opt")
                 }
@@ -84,7 +84,8 @@ namespace Aula1204Exercicio
             public void atualizaSituacao()
             {
                 Console.WriteLine("Situação do elevador: ");
-                Console.WriteLine($"Quantidade de pessoas no elevador: {this.pessoasDentro}; Andar atual: {this.terreo}");
+                Console.WriteLine($"Quantidade de pessoas no elevador: {this.pessoasDentro}; Andar atual: {this.andarAtual}");
+                Console.WriteLine($"Capacidade máxima: {this.totalPessoas} pessoas; Andares: {this.totalAndares}");
             }
 
             public void finalizar()
@@ -98,18 +99,22 @@ namespace Aula1204Exercicio
         {
             int capacidadeElevador = 0;
             int andares = 0;
- 
+
             do
             {
+
+
                 try
                 {
                     Console.Write("Capacidade do elevador: ");
                     capacidadeElevador = int.Parse(Console.ReadLine());
-                }catch (Exception e)
-                {
-                    Console.WriteLine("Valor inserido inválido. Favor inserir apenas números inteiros e positivos.");
                 }
-            }while (capacidadeElevador <= 0);
+                catch (Exception e)
+                {
+                    Console.WriteLine("Valor inserido inválido. Insira novamente");
+                }
+            } while (capacidadeElevador <= 0);
+
 
             do
             {
@@ -119,7 +124,7 @@ namespace Aula1204Exercicio
                     andares = int.Parse(Console.ReadLine());
                 }catch(Exception e)
                 {
-                    Console.WriteLine("Valor inserido inválido. Favor inserir apenas números inteiros e positivos.");
+                    Console.WriteLine("Valor inserido inválido. Insira novamente");
                 }
             }while (andares <= 0);
 
@@ -133,8 +138,15 @@ namespace Aula1204Exercicio
                 Console.WriteLine();    
                 elevador.atualizaSituacao();
                 Console.WriteLine();
+
                 Console.WriteLine("\tSelecione uma opção: \n1. Acrescentar pessoa elevador\n2. Remover pessoa do elevador\n3. Subir andar\n4. Descer andar\n5. Finalizar");
-                int opt = int.Parse(Console.ReadLine());
+                int opt = 0;
+
+                try
+                {
+                    opt = int.Parse(Console.ReadLine());
+                }catch(Exception e){}
+                  
                 Console.Clear();
                 switch (opt) {
                     case 1: 
@@ -154,12 +166,12 @@ namespace Aula1204Exercicio
                         break;
 
                     case 5:
-                        Console.WriteLine("Finalizando");
+                        Console.WriteLine("Finalizado");
                         verificar = false; // ou Environment.Exit(0);    -> caso usar while (true)
                         break;
 
                     default:
-                        Console.WriteLine("Opção inválida");
+                        Console.WriteLine("Opção inválida. Insira novamente");
                         break;
                 }
             }
